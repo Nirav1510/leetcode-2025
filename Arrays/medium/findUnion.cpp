a1a4ed9d8d68c5a2fb8784ab00f056b54ed3c477
Nirav Gusai <gusainirav1@gmail.com>
2024-12-20 23:24:06 +0530

    findUnion

---
+++ Arrays/medium/findUnion.cpp
@@ -0,0 +1,40 @@
+vector<int> findUnion(vector<int> &a, vector<int> &b) {
+    vector<int> ans;
+    int i = 0, j = 0, n = a.size(), m=b.size();
+    
+    while(i<n && j<m){
+        if(a[i] == b[j]){
+            if(ans.empty() || ans.back() != a[i]){
+                ans.push_back(a[i]);
+            }
+            i++;
+            j++;
+        }else if(a[i] > b[j]){
+            if(ans.empty() || ans.back() != b[j]){
+                ans.push_back(b[j]);
+            }
+            j++;
+        }else{
+            if(ans.empty() || ans.back() != a[i]){
+                ans.push_back(a[i]);
+            }
+            i++;
+        }
+    }
+    
+        while(i < n) {
+        if(ans.empty() || ans.back() != a[i]) {
+            ans.push_back(a[i]);
+        }
+        i++;
+    }
+
+    while(j < m) {
+        if(ans.empty() || ans.back() != b[j]) {
+            ans.push_back(b[j]);
+        }
+        j++;
+    }
+    
+    return ans;
+}
