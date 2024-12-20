5a1c3fa0f696d47a17a37cb13ce3a03000c0458b
Nirav Gusai <gusainirav1@gmail.com>
2024-12-21 01:01:10 +0530

    update

--- Arrays/medium/rotateMatrix.cpp
+++ Arrays/medium/rotateMatrix.cpp
@@ -3,9 +3,10 @@
 void rotate(vector<vector<int>>& mat) {
     int n = mat.size();
 
+    // transpose a matrix
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
-            swap(mat[i][j],mat[j][i]);
+            swap(mat[i][j], mat[j][i]);
         }
     }
 
