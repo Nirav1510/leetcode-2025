12132c088c77a26b000652c6f533caf10a48f16b
Nirav Gusai <gusainirav1@gmail.com>
2024-12-24 00:20:47 +0530

    update

--- Arrays/medium/findDuplicateNum.cpp
+++ Arrays/medium/findDuplicateNum.cpp
@@ -2,15 +2,15 @@ int findDuplicate(vector<int>& nums) {
     int slow=nums[0];
     int fast=nums[0];
     
-
     slow=nums[slow];
     fast=nums[nums[fast]];
 
     while(slow!=fast){
-            slow=nums[slow];
+        slow=nums[slow];
         fast=nums[nums[fast]];
 
     }
+
     slow=nums[0];
     while(slow!=fast){
         slow=nums[slow];
