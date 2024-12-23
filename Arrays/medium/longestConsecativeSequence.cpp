e107d320706a378b4d643fa9480df20d6bd33067
Nirav Gusai <gusainirav1@gmail.com>
2024-12-24 00:20:20 +0530

    longest-consecutive-sequence

---
+++ Arrays/medium/longestConsecativeSequence.cpp
@@ -0,0 +1,19 @@
+int longestConsecutive(vector<int>& nums) {
+    int ans = 0;
+    unordered_set<int> s(nums.begin(),nums.end());
+
+    for(int n : nums){
+        if(!s.count(n-1)){
+            int count = 1;
+            int currNum = n;
+
+            while(s.count(currNum + 1)){
+                currNum++;
+                count++;
+            }
+            ans=max(ans,count);
+        }
+    }
+
+    return ans;
+}
