bf09f98c4a076bbddea321be2bd85ee772db1e57
Nirav Gusai <gusainirav1@gmail.com>
2024-12-24 22:25:35 +0530

    maxProductSubArray using kedane's algorithm

--- Arrays/medium/maxProductSubArray.cpp
+++ Arrays/medium/maxProductSubArray.cpp
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/maximum-product-subarray/
+
 int maxProductSubArray(vector<int>& nums) {
     int ans = INT_MIN, preProd = 1, sufProd = 1, n=nums.size();
 
@@ -15,5 +17,21 @@ int maxProductSubArray(vector<int>& nums) {
 
         ans = max(ans, max(preProd, sufProd));
     }
+    return ans;
+}
+
+// Kedane's algorithm
+
+int maxProductSubArray2(vector<int>& nums) {
+    int ans = nums[0], currMax = nums[0], currMin = nums[0], n=nums.size();
+
+    for(int i=1;i<n;i++){
+        int temp = currMax;
+        currMax = max(nums[i], max(currMax*nums[i], currMin*nums[i]));
+        currMin = min(nums[i], min(temp*nums[i], currMin*nums[i]));
+
+        ans = max(ans, currMax);
+    }
+
     return ans;
 }
