e40cac7dda286ad19ecadd48ea1db85b24ecfc54
Nirav Gusai <gusainirav1@gmail.com>
2024-12-25 18:59:18 +0530

    search-in-rotated-sorted-array

---
+++ Binary-Search/medium/searchInRotatedSortedArray.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+int searchInRotatedSortedArray(vector<int>& a, int k) {
+    int low = 0, high = a.size()-1;
+
+    while(low<=high){
+        int mid = low + (high-low)/2;
+
+        if(a[mid] == k){
+            return mid;
+        }
+
+        if(a[low] <= a[mid]){
+            if(a[low]<=k && k <= a[mid]){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }else{
+            if(a[mid] <= k && k <= a[high]){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+    }
+    return -1;
+}
