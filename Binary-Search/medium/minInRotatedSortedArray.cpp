9b234e3d2f6435d4dcdeeaf7f66865f85341376e
Nirav Gusai <gusainirav1@gmail.com>
2024-12-28 19:52:42 +0530

    findMinAlternative

--- Binary-Search/medium/minInRotatedSortedArray.cpp
+++ Binary-Search/medium/minInRotatedSortedArray.cpp
@@ -16,3 +16,28 @@ int findMin(vector<int>& nums) {
 
     return nums[left]; 
 }
+
+int findMinAlternative(vector<int>& nums) {
+    int left = 0, n = nums.size();
+    int right = nums.size() - 1;
+
+    while (left <= right) {
+        if (nums[left] <= nums[right]) {
+            return nums[left];
+        }
+
+        int mid = left + (right - left) / 2;
+
+        int prev = (mid+n-1) % n;
+        int next = (mid+1) % n;
+
+        if (nums[mid] <= nums[prev] && nums[mid] <= nums[next]) {
+            return nums[mid];
+        } else if(nums[mid] >= nums[left]){
+            left = mid + 1;
+        } else{ 
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
