7be3a190b207919ebaa0cd443b20ca6387370c62
Nirav Gusai <gusainirav1@gmail.com>
2025-01-02 20:04:45 +0530

    palindrome-linked-list

---
+++ Linked-List/medium/isPalindromeList.cpp
@@ -0,0 +1,35 @@
+// https://leetcode.com/problems/palindrome-linked-list/
+
+ListNode *reverseList(ListNode* head){
+    ListNode *prev = NULL, *curr = head, *nextP;
+
+    while(curr){
+        nextP = curr->next;
+        curr->next = prev;
+
+        prev = curr;
+        curr = nextP;
+    }
+    return prev;
+}
+
+bool isPalindrome(ListNode* head) {
+    ListNode *slow = head, *fast = head;
+
+    while (fast && fast->next) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+
+    ListNode *secondHalf = reverseList(slow);
+    ListNode *itr1 = head, *itr2 = secondHalf;
+
+    while(itr2){
+        if(itr1->val != itr2->val){
+            return false;
+        }
+        itr1 = itr1->next;
+        itr2 = itr2->next;
+    }
+    return true;
+}
