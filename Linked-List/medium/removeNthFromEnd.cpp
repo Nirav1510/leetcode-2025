ae8518ad7fe4af7ba1f37833a24463a870c8d320
Nirav Gusai <gusainirav1@gmail.com>
2025-01-04 22:10:21 +0530

    remove-nth-node-from-end-of-list

---
+++ Linked-List/medium/removeNthFromEnd.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+ListNode* removeNthFromEnd(ListNode* head, int n) {
+    ListNode *slow = head, *fast = head;
+
+    while(n--){
+        fast = fast->next;
+    }
+
+    if (fast == NULL) return head->next;
+
+    while(fast && fast->next != NULL){
+        fast = fast->next;
+        slow = slow->next;
+    }
+
+    ListNode *toDelete = slow->next;
+    slow->next = slow->next->next;
+    delete(toDelete);
+
+    return head;
+}
