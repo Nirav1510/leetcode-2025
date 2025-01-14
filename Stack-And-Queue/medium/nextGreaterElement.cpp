4b907b34f6648ce6798ee58fafbd10318f665cc7
Nirav Gusai <gusainirav1@gmail.com>
2025-01-14 20:36:44 +0530

    next-larger-element to left

--- Stack-And-Queue/medium/nextGreaterElement.cpp
+++ Stack-And-Queue/medium/nextGreaterElement.cpp
@@ -1,11 +1,33 @@
 // https://www.geeksforgeeks.org/problems/next-larger-element-1587115620/1
 
+// next-larger-element to right
 vector<int> nextLargerElement(vector<int>& arr) {
     int n = arr.size();
     vector<int> ans(n, -1);
     stack<int> s;
     
     for(int i=n-1;i>=0;i--){
+        
+        while(!s.empty() && s.top() <= arr[i]){
+            s.pop();
+        }
+        
+        if(!s.empty() && s.top() > arr[i]){
+            ans[i] = s.top();
+        }
+        
+        s.push(arr[i]);
+    }
+    return ans;
+}
+
+// next-larger-element to left
+vector<int> nextLargerElementLeft(vector<int>& arr) {
+    int n = arr.size(); 
+    vector<int> ans(n, -1);
+    stack<int> s;
+    
+    for(int i=0;i<n;i++){
         
         while(!s.empty() && s.top() <= arr[i]){
             s.pop();
