ffd24e43587d38573bb95b50513fcc9798367947
Nirav Gusai <gusainirav1@gmail.com>
2025-01-21 01:47:11 +0530

    kth-symbol-in-grammar

---
+++ Recursion-And-Backtracking/easy/kthSymbolInGrammar.cpp
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/kth-symbol-in-grammar/
+
+int kthGrammar(int n, int k) {
+    if(n == 1 || k == 1) return 0;
+
+    int mid = pow(2, n-2);
+
+    if(k<=mid){
+        return kthGrammar(n-1, k);
+    }else{
+        return !kthGrammar(n-1, k-mid);
+    }
+}
