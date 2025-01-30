37b50f77d1a3561af2132383a3d56a008d2e9d1c
Nirav Gusai <gusainirav1@gmail.com>
2025-01-31 01:45:43 +0530

    top-k-frequent-elements

--- Heap/medium/topKFrequentElements.cpp
+++ Heap/medium/topKFrequentElements.cpp
@@ -1,5 +1,6 @@
 // https://leetcode.com/problems/top-k-frequent-elements/
 
+// Time Complexity: O(nlogk), Space Complexity: O(n)
 vector<int> topKFrequent(vector<int>& nums, int k) {
     vector<int> ans;
     unordered_map<int, int> mp;
@@ -22,5 +23,31 @@ vector<int> topKFrequent(vector<int>& nums, int k) {
         minHeap.pop();
         ans.push_back(num);
     }
+    return ans;
+}
+
+// bucket sort technique
+// Time Complexity: O(n), Space Complexity: O(n) 
+vector<int> topKFrequent(vector<int>& nums, int k) {
+    unordered_map<int, int> freqMap;
+    for (int num : nums) {
+        freqMap[num]++;
+    }
+
+    int n = nums.size();
+    vector<vector<int>> bucket(n + 1); // Frequency can be at most `n`
+    
+    for (auto& [num, freq] : freqMap) {
+        bucket[freq].push_back(num);
+    }
+
+    vector<int> ans;
+    for (int i = n; i >= 0 && ans.size() < k; --i) {
+        for (int num : bucket[i]) {
+            ans.push_back(num);
+            if (ans.size() == k) return ans;
+        }
+    }
+    
     return ans;
 }
