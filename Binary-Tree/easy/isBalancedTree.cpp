6159bc05a2224c50324ac159b46d02b31453fe73
Nirav Gusai <gusainirav1@gmail.com>
2025-02-09 01:39:24 +0530

    balanced-binary-tree

---
+++ Binary-Tree/easy/isBalancedTree.cpp
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/balanced-binary-tree/
+
+int getHeight(TreeNode *root){
+    if(!root) return 0;
+
+    int leftHeight = getHeight(root->left);
+    int rightHeight = getHeight(root->right);
+
+    if(leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1){
+        return -1;
+    }
+
+    return 1 + max(leftHeight, rightHeight);
+}
+
+bool isBalanced(TreeNode* root) {
+    return getHeight(root) != -1;
+}
