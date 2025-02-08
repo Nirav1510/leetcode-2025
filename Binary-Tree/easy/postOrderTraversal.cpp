6c1169cdc1e13670f7c21f2521f8db0a9c54658a
Nirav Gusai <gusainirav1@gmail.com>
2025-02-08 23:20:44 +0530

    binary-tree-postorder-traversal

---
+++ Binary-Tree/easy/postOrderTraversal.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/binary-tree-postorder-traversal/
+
+void getPostOrder(TreeNode *root, vector<int> &ans){
+    if(!root){
+        return;
+    }
+
+    getPostOrder(root->left, ans);
+    getPostOrder(root->right, ans);
+    ans.push_back(root->val);
+}
+
+vector<int> postorderTraversal(TreeNode* root) {
+    vector<int> ans;
+    getPostOrder(root, ans);
+    return ans;
+}
