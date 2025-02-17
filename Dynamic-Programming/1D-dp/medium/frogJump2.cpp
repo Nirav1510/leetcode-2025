7e4967be561b829361964ba51a91a532800d7491
Nirav Gusai <gusainirav1@gmail.com>
2025-02-17 23:46:36 +0530

    minimal-cost

---
+++ Dynamic-Programming/1D-dp/medium/frogJump2.cpp
@@ -0,0 +1,22 @@
+// https://www.geeksforgeeks.org/problems/minimal-cost/1
+
+int minimizeCost(int k, vector<int>& arr) {
+    int n = arr.size();
+    vector<int> dp(n, -1);
+    dp[0] = 0;
+    
+    for(int i=1;i<n;i++){
+        int jumpCost = INT_MAX;
+        
+        for(int j=1;j<=k;j++){
+            if(i-j >= 0){
+                int jump = dp[i-j] + abs(arr[i]-arr[i-j]);
+                jumpCost = min(jumpCost, jump);
+            }
+        }
+        
+        dp[i] = jumpCost;
+    }
+    
+    return dp[n-1];
+}
