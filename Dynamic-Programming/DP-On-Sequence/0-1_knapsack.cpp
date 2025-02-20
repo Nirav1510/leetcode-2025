2bcc0c06de76924339d6a306c747a1a249140467
Nirav Gusai <gusainirav1@gmail.com>
2025-02-21 00:40:49 +0530

    0-1-knapsack

--- Dynamic-Programming/DP-On-Sequence/0-1_knapsack.cpp
+++ Dynamic-Programming/DP-On-Sequence/0-1_knapsack.cpp
@@ -1,5 +1,6 @@
 // https://www.geeksforgeeks.org/problems/0-1-knapsack-problem0945/1
 
+// ------ Bottom Up Approach ------
 int knapSack(int c, vector<int> &val, vector<int> &wt) {
     int n = val.size();
     vector<vector<int>> dp(n+1, vector<int>(c+1, 0));
@@ -17,4 +18,28 @@ int knapSack(int c, vector<int> &val, vector<int> &wt) {
         }
     }
     return dp[n][c];
+}
+
+// ------ Top Down Approach -----
+int solve(int c, vector<int> &val, vector<int> &wt, int n, vector<vector<int>> &dp){
+    if(n == 0 || c == 0) return 0;
+    
+    if(dp[n][c] != -1) return dp[n][c];
+    
+    int exclude = solve(c, val, wt, n-1, dp);
+    
+    if(wt[n-1] <= c){
+        int include = val[n-1] + solve(c-wt[n-1], val, wt, n-1, dp);
+        return dp[n][c] = max(include, exclude);
+    }else{
+        return dp[n][c] = exclude;
+    }
+}
+
+int knapSack(int c, vector<int> &val, vector<int> &wt) {
+    int n = val.size();
+    vector<vector<int>> dp(n+1, vector<int>(c+1, -1));
+    
+    int ans = solve(c, val, wt, n, dp);
+    return ans;
 }
