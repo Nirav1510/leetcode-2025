6dc13f8459b8cef3d43518617be5727e2fb65276
Nirav Gusai <gusainirav1@gmail.com>
2025-02-20 21:31:10 +0530

    unique-paths

--- Dynamic-Programming/2D-dp/easy/uniquePaths.cpp
+++ Dynamic-Programming/2D-dp/easy/uniquePaths.cpp
@@ -25,6 +25,25 @@ int uniquePaths(int m, int n) {
     return dp[m-1][n-1];
 }
 
+// ------- Bottom up approach ------
+int helper(int i, int j, vector<vector<int>>& dp){
+    if(i == 0 && j == 0) return 1;
+    if(i < 0 || j < 0) return 0;
+
+    if(dp[i][j] != -1) return dp[i][j];
+
+    int down = helper(i-1, j, dp);
+    int right = helper(i, j-1, dp);
+
+    return dp[i][j] = down + right;
+}
+
+int uniquePaths(int m, int n) {
+    vector<vector<int>> dp(m, vector<int>(n, -1));
+
+    return helper(m-1, n-1, dp);
+}
+
 // ------ Mathmatical solution ------
 int uniquePaths(int m, int n) {
     int N = m+n-2;
