787de03a1cf840ae00ad369a2de0340cef213236
Nirav Gusai <gusainirav1@gmail.com>
2025-02-28 15:30:50 +0530

    delete-operation-for-two-strings

---
+++ Dynamic-Programming/DP-On-LCS/minDeletionToMakeBothStringEqual.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/delete-operation-for-two-strings/
+
+int minDistance(string a, string b) {
+    int n = a.size(), m = b.size();
+    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<m+1;j++){
+            if(a[i-1] == b[j-1]){
+                dp[i][j] = 1 + dp[i-1][j-1];
+            }else{
+                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+            }
+        }
+    }
+
+    int ans = n+m-(2*dp[n][m]);
+    return ans;
+}
