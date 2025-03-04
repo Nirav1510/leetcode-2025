a59f1b027a1b67e7d02bffa4454f89ea85df569a
Nirav Gusai <gusainirav1@gmail.com>
2025-03-04 15:01:24 +0530

    distinct-subsequences

--- Dynamic-Programming/DP-On-LCS/distinctSubsequence.cpp
+++ Dynamic-Programming/DP-On-LCS/distinctSubsequence.cpp
@@ -10,12 +10,13 @@ int numDistinct(string a, string b) {
 
     for(int i=1;i<n+1;i++){
         for(int j=1;j<m+1;j++){
-            dp[i][j] = dp[i-1][j];
-
             if(a[i-1] == b[j-1]){
-                dp[i][j] += dp[i-1][j-1];
+                dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+            }else{
+                dp[i][j] = dp[i-1][j];
             }
         }
     }
+    
     return dp[n][m];
 }
