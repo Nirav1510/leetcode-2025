de647c9d0ba41877e23e23302174ff679243293e
Nirav Gusai <gusainirav1@gmail.com>
2025-03-04 14:51:46 +0530

    distinct-subsequences

---
+++ Dynamic-Programming/DP-On-LCS/distinctSubsequence.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/distinct-subsequences/
+
+int numDistinct(string a, string b) {
+    int n = a.size(), m = b.size();
+    vector<vector<unsigned long long int>> dp(n+1, vector<unsigned long long int>(m+1, 0));
+
+    for(int i=0;i<n+1;i++){
+        dp[i][0] = 1;
+    }
+
+    for(int i=1;i<n+1;i++){
+        for(int j=1;j<m+1;j++){
+            dp[i][j] = dp[i-1][j];
+
+            if(a[i-1] == b[j-1]){
+                dp[i][j] += dp[i-1][j-1];
+            }
+        }
+    }
+    return dp[n][m];
+}
