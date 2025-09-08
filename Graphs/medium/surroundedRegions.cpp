06ad9d4e526e214339ce85710e8e8f01903f92fe
Nirav Gusai <gusainirav1@gmail.com>
2025-09-09 01:30:29 +0530

    surroundedRegions

--- Graphs/medium/surroundedRegions.cpp
+++ Graphs/medium/surroundedRegions.cpp
@@ -17,6 +17,32 @@ void dfs(int r, int c, vector<vector<bool>> &vis, vector<vector<char>>& mat){
     }
 }
 
+void bfs(int r, int c, vector<vector<bool>> &vis, vector<vector<char>>& mat) {
+    int n = mat.size(), m = mat[0].size();
+    queue<pair<int,int>> q;
+    q.push({r, c});
+    vis[r][c] = true;
+
+    int dx[] = {-1, 0, 1, 0};
+    int dy[] = {0, -1, 0, 1};
+
+    while (!q.empty()) {
+        auto [x, y] = q.front();
+        q.pop();
+
+        for (int i = 0; i < 4; i++) {
+            int nRow = x + dx[i];
+            int nCol = y + dy[i];
+
+            if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m &&
+                !vis[nRow][nCol] && mat[nRow][nCol] == 'O') {
+                vis[nRow][nCol] = true;
+                q.push({nRow, nCol});
+            }
+        }
+    }
+}
+
 void surroundedRegions(vector<vector<char>>& mat) {
     int n = mat.size(), m = mat[0].size();
     vector<vector<bool>> vis(n, vector<bool>(m, false));
@@ -25,10 +51,12 @@ void surroundedRegions(vector<vector<char>>& mat) {
     for(int j=0;j<m;j++){
         if(!vis[0][j] && mat[0][j] == 'O'){
             dfs(0, j, vis, mat);
+            // bfs(0, j, vis, mat);
         }
         
         if(!vis[n-1][j] && mat[n-1][j] == 'O'){
             dfs(n-1, j, vis, mat);
+            // bfs(n-1, j, vis, mat);
         }
     }
     
@@ -36,10 +64,12 @@ void surroundedRegions(vector<vector<char>>& mat) {
     for(int i=0;i<n;i++){
         if(!vis[i][0] && mat[i][0] == 'O'){
             dfs(i, 0, vis, mat);
+            // bfs(i, 0, vis, mat);
         }
         
         if(!vis[i][m-1] && mat[i][m-1] == 'O'){
             dfs(i, m-1, vis, mat);
+            // bfs(i, m-1, vis, mat);
         }
     }
     
